8c67357f515761fd9f8ef02a3245a2e3d46de76d
Raphael De Pieri Poi Fulgêncio <raphael.pieri@inlog.com.br>
2019-10-31 18:20:39 -0300

    pARTE 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { PetsPageComponent } from './pets-page/pets-page.component';
-import { ProductsPageComponent } from './products-page/products-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
+import { ProductCartComponent } from './components/store/product-cart/product-cart.component';
+
 
 @NgModule({
   declarations: [
@@ -20,10 +23,13 @@ import { CartPageComponent } from './cart-page/cart-page.component';
     SignupPageComponent,
     PetsPageComponent,
     ProductsPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    FramePageComponent,
+    ProductCartComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-products-page',
+  templateUrl: './products-page.component.html'
+})
+export class ProductsPageComponent implements OnInit {
+  public products$: Observable<any[]>;
+
+  constructor(private data: DataService) 
+  { 
+
+  }
+
+  ngOnInit() {
+    this.products$ = this.data.getProducts();
+  }
+
+}
